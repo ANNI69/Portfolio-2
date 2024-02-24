2cab85751847a8544f708b00d6efaa9d90ee8f32
Anii <aniruddh.nagare@vpt.edu.in>
2024-02-24 22:34:02 +0530

    esss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Aniruddh Nagare",
-  description: "Developerand Creator"
+  description: "Developerand Creator",
 };
 
 export default function RootLayout({
